d9564f214452cd97a5a209d29c94215c1417a2aa
Jia Rong Gan <jiaronggan21@gmail.com>
2024-02-29 17:17:31 -0600

    completed search 1 and 2

--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,3 +1,4 @@
+import { Stack } from "../common/stack";
 import { GNodeStr } from "../graph/graph";
 
 /** Return array of nodes, in DFS order (recursive version)  */
@@ -6,16 +7,47 @@ function rDfs(
     start: GNodeStr,
     result: string[] = [],
     visited = new Set([start])): string[] {
-  return ["todo"];
+  //loop through node's adjacent
+  //save whomever with have visited
+  //if node is not in visited, push node to result
+  //recurse back
+  result.push(start.value);
+
+  for (const adj of start.adjacent){
+    if (!visited.has(adj)){
+      visited.add(adj)
+      rDfs(adj,result,visited)
+    }
+  }
+  console.log("result", result)
+  return result;
 }
+
 
 /** Return array of nodes, in DFS order (iterative version)  */
 
 function iDfs(start: GNodeStr): string[] {
-  return ["todo"];
+  let stack = new Stack([start]);
+  let result: string[] = [];
+  let visited = new Set();
+
+
+  while(!stack.isEmpty()){
+    let curNode = stack.pop();
+    visited.add(curNode);
+    result.push(curNode.value);
+
+    for (const adj of curNode.adjacent){
+      if (!visited.has(adj)){
+        visited.add(adj)
+        stack.push(adj)
+      }
+    }
+  }
+  return result;
 }
 
-/** Return array of nodes, in BFS order (recursive version)  */
+/** Return array of nodes, in BFS order (iterative version)  */
 
 function bfs(start: GNodeStr): string[] {
   return ["todo"];
